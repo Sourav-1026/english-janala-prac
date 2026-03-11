2b3a4425ae9e6f4a862315bb7d007064a0eaaa50
Sourav-1026 <faruqueomar740@gmail.com>
2026-03-12 00:06:01 +0600

    functionality has added to level four and level seven

--- script/index.js
+++ script/index.js
@@ -19,13 +19,27 @@ const loadLevelWord = async (id) => {
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+      <div class="text-center col-span-full py-10 font-bangla space-y-3">
+        <div class = "flex justify-center mb-3">
+          <img src="./assets/alert-error.png" alt="" srcset="">
+        </div>
+        <p class="text-[#79716B] font-semibold">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h1 class="text-[34px] font-medium">নেক্সট Lesson এ যান</h1>
+      </div>
+    `;
+    return;
+  }
+
   for (let word of words) {
     const wordDiv = document.createElement("div");
     wordDiv.innerHTML = `
       <div class="bg-white rounded-md shadow-sm py-6 px-2 text-center space-y-5">
         <h2 class="font-bold text-2xl">${word.word}</h2>
         <p class="font-medium">Meaning /Pronounciation</p>
-        <h2 class="font-semibold font-bangla text-2xl">"${word.meaning} / ${word.pronunciation}"</h2>
+        <h2 class="font-semibold font-bangla text-2xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation}"</h2>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF]/80"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF]/80"><i class="fa-solid fa-volume-high"></i></button>
